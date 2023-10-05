165472af87dde45652a1ccbccb7b637ce99c9794
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-05 16:40:30 -0300

    Definiendo variable email

--- PruebaLogin.js
+++ PruebaLogin.js
@@ -4,8 +4,9 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const LoginScreen = () => {
-  /*
+
   const [email, setEmail] = useState('');
+  /*
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
