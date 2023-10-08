5f321c0b024b9610112f43f22e1c561b1aaa9bdb
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-08 19:13:25 -0300

    Definiendo links

--- footer.jsx
+++ footer.jsx
@@ -2,5 +2,7 @@ import { icon, logo } from './assets';
 import { FaInstagram,FaFacebook,FaXTwitter,FaWhatsApp} from "react-icons/fa6";
 
 const Footer = () => {
-    
+    const links = [
+        
+    ]
 }
