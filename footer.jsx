a654816f175f18e87a19f2be98da88f871031c3d
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-09 16:52:55 -0300

    Definiendo quinto  links

--- footer.jsx
+++ footer.jsx
@@ -14,8 +14,7 @@ const Footer = () => {
             {label : 'Soporte' , key: 'header-2'},
             {label : 'Centro de ayuda' , key: 'item-2-1'},
             {label : ' Política de Privacidad' , key: 'item-2-2'},
-            
-           
+            {label : 'Estado del servicio' , key: 'item-2-3'},  
         ]
     ]
 }
