4e1308280451b1c846f39d964b7fd9e9958801ff
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-09 17:18:32 -0300

    Definiendo icono 3

--- footer.jsx
+++ footer.jsx
@@ -37,6 +37,7 @@ const Footer = () => {
              <div className="footer-icons">
                <FaInstagram/>
                <FaFacebook/>
+               <FaWhatsApp/>
 
              </div>
             </div>
